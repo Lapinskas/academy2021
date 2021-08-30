ea0986cfbf2ba5e70ba2c65a10011b433954a6b9
Vladas Lapinskas <vlapinskas@pdmfc.com>
2021-08-30 13:41:05 +0100

    Quasar table

--- resources/academy2021/src/pages/Index.vue
+++ resources/academy2021/src/pages/Index.vue
@@ -1,17 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-table
+      title="Products"
+      row-key="name"
+      :columns="columns"
+      :rows="rows"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  name: 'PageIndex',
+  setup () {
+    return {
+      columns: [
+        { name: 'name',           label: 'Name',            field: 'name' },
+        { name: 'description',    label: 'Description',     field: 'description' },
+        { name: 'price',          label: 'Price',           field: 'price' },
+      ],
+      rows: [
+        { name: 'Test', description: 'Quasar test', price: 'Priceless' },
+        { name: 'Product', description: 'Just for fun', price: '500.00' },
+      ]
+    }
+  }
+}
 </script>
